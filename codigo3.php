5242a3b9d2814333062fa31230bf1d3eba29520e
Dario Diaz <dariodiaz2006@gmail.com>
2024-04-03 09:00:49 -0300

    Variables

---
+++ codigo3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $nombre = 'Manolo'; //Texto. Puede ser con comillas simples o dobles (String)
+    $edad = 31; //Enteros (Integer)
+    $altura = 1.72; //Decimales, usando el punto en lugar de la coma (Float)
+    $mayorEdad = true; //Verdad o falso (Boolean)
+    ?>
+</body>
+</html>
