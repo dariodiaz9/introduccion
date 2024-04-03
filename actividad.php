aadd27956e8dac8cb6e185eec4fd0d4d77d668ff
Dario Diaz <dariodiaz2006@gmail.com>
2024-04-03 09:18:46 -0300

    Terminado el 3/4/24

---
+++ actividad.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nombreCompleto = "Juan Pérez"
+    $Edad = 25; //La variable no deberia empezar con mayúscula
+    $estAtura = 1.75; //deberia ser estatura pero de todos modos esta bien como variable
+    $numero = "10"; //No deberia haber comillas si su proposito es el de ser un Integer
+    Apellido = 'Fernández'; //Si fuera una variable (la cual no es porque falta $) no deberia empezar con mayúscula
+    pais_de_origen= "Uruguay" //Falta $ y ;
+    $salario = $2000; //Deberia ser un string
+    ?>
+</body>
+</html>
