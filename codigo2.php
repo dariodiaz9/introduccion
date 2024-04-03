38766bf59e8cc6a48ca2da4940bac8bc0c5c761f
Dario Diaz <dariodiaz2006@gmail.com>
2024-04-03 08:48:33 -0300

    Actualizado con comentarios

--- codigo2.php
+++ codigo2.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Mi segundo archivo en PHP</h1>
-    <?php
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Mi segundo archivo en PHP</h1>
+        <?php
+    // Comentario 1
+    /* Comentario 2
+    qwerty */
+    # Comentario 3
     echo <<<END
     En un mundo conectado,
     donde cada segundo se envía millones de correos
