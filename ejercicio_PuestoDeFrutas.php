f04fce1cf9efd495e9875df659b9724bcc6d02d5
Darío Díaz <dariodiaz2006@gmail.com>
2024-04-17 09:17:20 -0300

    Ejercicio del 17 de abril, vencimiento: 24 de abril 0:00am

---
+++ ejercicio_PuestoDeFrutas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Verduleria Pepe</h1>
+    <?php
+    $bananas = 1;
+    $manzanas = 1;
+    ?>
+</body>
+</html>
