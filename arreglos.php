934d69a645584cb5bcbe6a8534446010d4fb69a6
Darío Díaz <alumno@isbo.edu.uy>
2024-04-24 09:00:01 -0300

    Clase 24/4/24

--- arreglos.php
+++ arreglos.php
@@ -8,4 +8,24 @@ $semana = [
   'Sabado',
   'Domingo',  
 ];
+
+var_dump($semana);
+echo "<br/>".$semana[0];
+echo"<br/>".$semana[10];
+
+$planetas = [];
+$planetas[] = 'Mercurio';
+$planetas[] = 'Venus';
+$planetas[] = 'Tierra';
+$planetas[] = 'Marte';
+$planetas[] = 'Jupiter';
+$planetas[] = 'Saturno';
+$planetas[] = 'Urano';
+$planetas[] = 'Neptuno';
+echo count($planetas);//3
+var_dump($planetas);
+
+foreach ($planetas as $astro){
+  echo "<br>". $astro . "<br>";
+}
 ?>
